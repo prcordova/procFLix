63730672404ed6c4e4ed6f4444fae3d44e496a5c
Pedro Ricardo Oliveira Córdova <43557558+prcordova@users.noreply.github.com>
2021-11-05 05:19:16 -0300

    home page created , buttons, components

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Header.css'
+
+export default ({ black }) => {
+  return (
+    <header className={black ? 'black' : ''}>
+      <div className="header--logo">
+        <a href="/">
+          <img src="https://logosmarcas.net/wp-content/uploads/2020/04/Netflix-Logo.png" alt="" />
+        </a>
+      </div>
+      <div className="header--user">
+        <a href="/">
+          <img src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/1bdc9a33850498.56ba69ac2ba5b.png" alt="" />
+        </a>
+      </div>
+    </header>
+  )
+}
